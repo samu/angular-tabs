b2fa37f4e32dc78bfb8ae7bdc08cf4df5b52b2a2
Samuel Mueller <mueller.samu@gmail.com>
2013-06-09 21:28:46 +0200

    compiled newest version

--- angular-tabs.js
+++ angular-tabs.js
@@ -5,62 +5,97 @@
 
   angular.module("angular-tabs").service("angularTabs", [
     function() {
-      var angularTabInstances;
+      var activeTabs;
 
-      angularTabInstances = {};
+      activeTabs = {};
       return {
         getActiveTab: function(id) {
-          return angularTabInstances[id];
+          return activeTabs[id];
         },
         setActiveTab: function(id, title) {
-          return angularTabInstances[id] = title;
+          return activeTabs[id] = title;
         }
       };
     }
   ]);
 
   angular.module("angular-tabs").directive("angularTabs", [
-    "angularTabs", function(angularTabs) {
+    "$compile", "angularTabs", function($compile, angularTabs) {
+      var compileTabsBody, createControl, createTab;
+
+      compileTabsBody = function(divs) {
+        var div, title, titles, _i, _len;
+
+        titles = [];
+        for (_i = 0, _len = divs.length; _i < _len; _i++) {
+          div = divs[_i];
+          div = angular.element(div);
+          title = div.attr("title");
+          titles.push(title);
+          div.attr("ng-show", "isActive('" + title + "')");
+        }
+        return titles;
+      };
+      createControl = function(classes, titles) {
+        var tabs, title, _i, _len;
+
+        tabs = angular.element("<ul></ul>");
+        if (!classes) {
+          tabs.addClass("nav");
+          tabs.addClass("nav-tabs");
+        } else {
+          tabs.addClass(classes);
+        }
+        for (_i = 0, _len = titles.length; _i < _len; _i++) {
+          title = titles[_i];
+          tabs.append(createTab(title));
+        }
+        return tabs;
+      };
+      createTab = function(title) {
+        var a, li;
+
+        li = angular.element("<li></li>");
+        li.attr("ng-class", "{active: isActive('" + title + "')}");
+        li.append(a = angular.element("<a>" + title + "</a>"));
+        a.attr("href", "");
+        a.attr("ng-click", "setActive('" + title + "')");
+        return li;
+      };
       return {
         restrict: "E",
         scope: true,
         compile: function(element, attributes, transclude) {
-          var a, div, divs, i, li, tabs, title, _i, _ref;
+          var control, divs, id, tabControls, titles;
 
-          tabs = angular.element("<ul></ul>");
-          tabs.addClass("nav");
-          tabs.addClass("nav-tabs");
+          id = attributes.id;
           divs = element.children("div");
-          for (i = _i = 0, _ref = divs.length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
-            div = angular.element(divs[i]);
-            title = div.attr("title");
-            if (!title) {
-              title = i;
-            }
-            div.attr("ng-show", "tabIsActive('" + title + "')");
-            li = angular.element("<li></li>");
-            li.attr("ng-class", "{active: tabIsActive('" + title + "')}");
-            a = angular.element("<a>" + title + "</a>");
-            a.attr("href", "");
-            a.attr("ng-click", "setActiveTab('" + title + "')");
-            li.append(a);
-            tabs.append(li);
+          titles = compileTabsBody(divs);
+          tabControls = angular.element("angular-tabs-control[for=" + id + "]");
+          if (tabControls.length <= 0) {
+            control = createControl(attributes["class"], titles);
+            element.prepend(control);
           }
-          element.prepend(tabs);
           return {
             post: function($scope, $element, $attributes) {
-              var activeTab, id;
+              var tabControl, _i, _len;
 
-              id = $attributes.id;
-              activeTab = $attributes.activeTab;
-              $scope.setActiveTab = function(title) {
-                $scope.activeTab = title;
+              if (tabControls.length > 0) {
+                for (_i = 0, _len = tabControls.length; _i < _len; _i++) {
+                  tabControl = tabControls[_i];
+                  tabControl = angular.element(tabControl);
+                  control = createControl(tabControl.attr("class"), titles);
+                  $compile(control)($scope);
+                  tabControl.append(control);
+                }
+              }
+              $scope.setActive = function(title) {
                 return angularTabs.setActiveTab(id, title);
               };
-              $scope.tabIsActive = function(title) {
-                return $scope.activeTab === title;
+              $scope.isActive = function(title) {
+                return angularTabs.getActiveTab(id) === title;
               };
-              return $scope.setActiveTab(activeTab);
+              $scope.setActive($attributes.activeTab);
             }
           };
         }
